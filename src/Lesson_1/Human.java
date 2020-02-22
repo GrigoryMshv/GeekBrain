463800f45f607d33e909458b9f096792debe54bc
Grigory Moshev <grogory.mshv@gmail.com>
2020-02-23 00:47:34 +0300

    Lesson_1

---
+++ src/Lesson_1/Human.java
@@ -0,0 +1,12 @@
+package Lesson_1;
+
+public class Human {
+    void run() {
+        System.out.println("Человек может бежать");
+    }
+        void jump() {
+        System.out.println("Человек может прыгать");
+
+
+    }
+}
---
+++ src/Lesson_1/Main.java
@@ -0,0 +1,10 @@
+package Lesson_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Human hum = new Human();
+    hum.run();
+    hum.jump();
+    }
+}
